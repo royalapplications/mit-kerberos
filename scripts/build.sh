352b2aebc9c14ff1ca7c9fbb298a1220adf0676d
Felix Deimel <felix@lemonmojo.com>
2024-05-14 14:57:53 +0200

    macOS Universal Build

--- scripts/build.sh
+++ scripts/build.sh
@@ -53,6 +53,24 @@ SRC_DIR="${BUILD_DIR}/src"
 # TODO
 OPENSSL_BASE_DIR="/Users/fx/dev/royalapps/freerdpkit/bin/OpenSSL/openssl-3.2.1"
 
+merge_static_libs() {
+  local target_dir="$1"
+  local output_file="$2"
+
+  xcrun libtool -static \
+    "${target_dir}/libkrb5.a" \
+    "${target_dir}/libverto.a" \
+    "${target_dir}/libkrb5support.a" \
+    "${target_dir}/libkrad.a" \
+    "${target_dir}/libkdb5.a" \
+    "${target_dir}/libkadm5clnt_mit.a" \
+    "${target_dir}/libk5crypto.a" \
+    "${target_dir}/libgssrpc.a" \
+    "${target_dir}/libgssapi_krb5.a" \
+    "${target_dir}/libcom_err.a" \
+    -o "${output_file}"
+}
+
 build() {
   local os="$1"
   local arch="$2"
@@ -69,7 +87,7 @@ build() {
   local install_dir="${build_target_dir}/bin"
 
   local cflags="-DDEFAULT_RDNS_LOOKUP=0"
-  local ldflags="-framework Kerberos"
+  local ldflags=""
   local target=""
 
   local os_name=""
@@ -84,12 +102,17 @@ build() {
     target="${arch}-apple-darwin"
 
     cflags="${cflags} -isysroot ${sdk_root} -mmacosx-version-min=${MACOS_VERSION_MIN} -target ${target} -I ${openssl_include_dir}"
-    ldflags="${ldflags} -isysroot ${sdk_root} -mmacosx-version-min=${MACOS_VERSION_MIN} -L ${openssl_lib_dir}"
+    ldflags="-framework Kerberos -isysroot ${sdk_root} -mmacosx-version-min=${MACOS_VERSION_MIN} -L ${openssl_lib_dir}"
   elif [ "$os" = "iphoneos" ]; then
     os_name="iOS"
     local sdk_root=$(xcrun --sdk iphoneos --show-sdk-path)
 
     target="${arch}-apple-ios"
+
+    # Manually set the correct values for configure checks that MIT Kerberos won't be able to perform because we're cross-compiling.
+    export krb5_cv_attr_constructor_destructor=yes
+    export ac_cv_func_regcomp=yes
+    export ac_cv_printf_positional=yes
 
     cflags="${cflags} -isysroot ${sdk_root} -miphoneos-version-min=${IOS_VERSION_MIN} -target ${target} -I ${openssl_include_dir}"
     ldflags="${ldflags} -isysroot ${sdk_root} -miphoneos-version-min=${IOS_VERSION_MIN} -L ${openssl_lib_dir}"
@@ -98,6 +121,11 @@ build() {
     local sdk_root=$(xcrun --sdk iphonesimulator --show-sdk-path)
 
     target="${arch}-apple-ios-simulator"
+
+    # Manually set the correct values for configure checks that MIT Kerberos won't be able to perform because we're cross-compiling.
+    export krb5_cv_attr_constructor_destructor=yes
+    export ac_cv_func_regcomp=yes
+    export ac_cv_printf_positional=yes
 
     cflags="${cflags} -isysroot ${sdk_root} -miphoneos-version-min=${IOS_VERSION_MIN} -target ${target} -I ${openssl_include_dir}"
     ldflags="${ldflags} -isysroot ${sdk_root} -miphoneos-version-min=${IOS_VERSION_MIN} -L ${openssl_lib_dir}"
@@ -136,57 +164,59 @@ build() {
   echo "Making MIT Kerberos for ${os_name} ${arch}, install"
   make install
 
-  # echo "Removing all dylibs"
-  # find "${build_target_dir}/lib" -iname '*.dylib' -delete
+  echo "Merging static libraries for ${os_name} ${arch}"
 
-  # echo "Removing pkgconfig dir"
-  # rm -fr "${build_target_dir}/lib/pkgconfig"
+  merge_static_libs \
+    "${install_dir}/lib" \
+    "${install_dir}/lib/libMITKerberos.a"
 }
 
-# make_universal_lib() {
-#   local file_name="$1"
-#   local target_dir="$2"
-#   local source_dir_1="$3"
-#   local source_dir_2="$4"
+make_universal_lib() {
+  local file_name="$1"
+  local target_dir="$2"
+  local source_dir_1="$3"
+  local source_dir_2="$4"
 
-#   if [[ -d "${target_dir}" ]]; then
-#     rm -rf "${target_dir}"
-#   fi
+  if [[ -d "${target_dir}" ]]; then
+    rm -rf "${target_dir}"
+  fi
 
-#   mkdir -p "${target_dir}"
+  mkdir -p "${target_dir}"
 
-#   echo "Making universal binary at ${target_dir}/${file_name} out of ${source_dir_1}/${file_name} and ${source_dir_2}/${file_name}"
+  echo "Making universal binary at ${target_dir}/${file_name} out of ${source_dir_1}/${file_name} and ${source_dir_2}/${file_name}"
 
-#   lipo -create \
-#     "${source_dir_1}/${file_name}" \
-#     "${source_dir_2}/${file_name}" \
-#     -output "${target_dir}/${file_name}"
-# }
+  lipo -create \
+    "${source_dir_1}/${file_name}" \
+    "${source_dir_2}/${file_name}" \
+    -output "${target_dir}/${file_name}"
+}
 
 TARGET_DIR_MACOS_ARM64="${TARGET_DIR}/macosx-arm64"
 TARGET_DIR_MACOS_X86="${TARGET_DIR}/macosx-x86_64"
-# TARGET_DIR_IOS_ARM64="${TARGET_DIR}/iphoneos"
-# TARGET_DIR_IOS_SIMULATOR_ARM64="${TARGET_DIR}/iphonesimulator-arm64"
-# TARGET_DIR_IOS_SIMULATOR_X86="${TARGET_DIR}/iphonesimulator-x86_64"
+TARGET_DIR_IOS_ARM64="${TARGET_DIR}/iphoneos"
+TARGET_DIR_IOS_SIMULATOR_ARM64="${TARGET_DIR}/iphonesimulator-arm64"
+TARGET_DIR_IOS_SIMULATOR_X86="${TARGET_DIR}/iphonesimulator-x86_64"
 
 build "macosx" "aarch64" "${TARGET_DIR_MACOS_ARM64}"
 build "macosx" "x86_64" "${TARGET_DIR_MACOS_X86}"
+
+# TODO: For iOS
 # build "iphoneos" "aarch64" "${TARGET_DIR_IOS_ARM64}"
 # build "iphonesimulator" "aarch64" "${TARGET_DIR_IOS_SIMULATOR_ARM64}"
 # build "iphonesimulator" "x86_64" "${TARGET_DIR_IOS_SIMULATOR_X86}"
 
-# TARGET_DIR_MACOS_UNIVERSAL="${TARGET_DIR}/macosx"
+TARGET_DIR_MACOS_UNIVERSAL="${TARGET_DIR}/macosx"
 # TARGET_DIR_IOS_SIMULATOR_UNIVERSAL="${TARGET_DIR}/iphonesimulator"
 
-# make_universal_lib \
-#   "libmitkerberos.a" \
-#   "${TARGET_DIR_MACOS_UNIVERSAL}/lib" \
-#   "${TARGET_DIR_MACOS_ARM64}/lib" \
-#   "${TARGET_DIR_MACOS_X86}/lib"
+make_universal_lib \
+  "libMITKerberos.a" \
+  "${TARGET_DIR_MACOS_UNIVERSAL}/lib" \
+  "${TARGET_DIR_MACOS_ARM64}/bin/lib" \
+  "${TARGET_DIR_MACOS_X86}/bin/lib"
 
-# cp -r \
-#   "${TARGET_DIR_MACOS_ARM64}/include" \
-#   "${TARGET_DIR_MACOS_UNIVERSAL}/include"
+cp -r \
+  "${TARGET_DIR_MACOS_ARM64}/bin/include" \
+  "${TARGET_DIR_MACOS_UNIVERSAL}/include"
 
 # make_universal_lib \
 #   "libmitkerberos.a" \
@@ -198,17 +228,21 @@ build "macosx" "x86_64" "${TARGET_DIR_MACOS_X86}"
 #   "${TARGET_DIR_IOS_SIMULATOR_ARM64}/include" \
 #   "${TARGET_DIR_IOS_SIMULATOR_UNIVERSAL}/include"
 
-# echo "Creating Apple-Universal XCFramework at ${TARGET_DIR}/MITKerberos.xcframework"
+echo "Creating Apple-Universal XCFramework at ${TARGET_DIR}/MITKerberos.xcframework"
 
 # xcodebuild -create-xcframework \
-#   -library "${TARGET_DIR_MACOS_UNIVERSAL}/lib/libmitkerberos.a" \
-#   -library "${TARGET_DIR_IOS_ARM64}/lib/libmitkerberos.a" \
-#   -library "${TARGET_DIR_IOS_SIMULATOR_UNIVERSAL}/lib/libmitkerberos.a" \
+#   -library "${TARGET_DIR_MACOS_UNIVERSAL}/bin/lib/libmitkerberos.a" \
+#   -library "${TARGET_DIR_IOS_ARM64}/bin/lib/libmitkerberos.a" \
+#   -library "${TARGET_DIR_IOS_SIMULATOR_UNIVERSAL}/bin/lib/libmitkerberos.a" \
 #   -output "${TARGET_DIR}/MITKerberos.xcframework"
 
-# echo "Codesigning XCFramework"
+xcodebuild -create-xcframework \
+  -library "${TARGET_DIR_MACOS_UNIVERSAL}/bin/lib/libMITKerberos.a" \
+  -output "${TARGET_DIR}/MITKerberos.xcframework"
 
-# codesign \
-#     --force --deep --strict \
-#     --sign "${CODESIGN_ID}" \
-#     "${TARGET_DIR}/MITKerberos.xcframework"
+echo "Codesigning XCFramework"
+
+codesign \
+    --force --deep --strict \
+    --sign "${CODESIGN_ID}" \
+    "${TARGET_DIR}/MITKerberos.xcframework"
