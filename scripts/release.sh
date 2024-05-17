a67e10aa7fd2a256350b945ba1737fa22648eb98
Felix Deimel <felix@lemonmojo.com>
2024-05-17 11:55:37 +0200

    Release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,12 +5,12 @@ set -e
 SCRIPT_PATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo "Script Path: ${SCRIPT_PATH}"
 
-if [[ -z $OPENSSL_VERSION ]]; then
-  echo "OPENSSL_VERSION not set; aborting"
+if [[ -z $MITKERBEROS_VERSION ]]; then
+  echo "MITKERBEROS_VERSION not set; aborting"
   exit 1
 fi
 
-BUILD_DIR="${SCRIPT_PATH}/../build/openssl-${OPENSSL_VERSION}/build"
+BUILD_DIR="${SCRIPT_PATH}/../build/mitkerberos-${MITKERBEROS_VERSION}"
 echo "Build Path: ${BUILD_DIR}"
 
 if [[ ! -d "${BUILD_DIR}" ]]; then
@@ -20,12 +20,12 @@ fi
 
 pushd "${BUILD_DIR}"
 
-echo "Creating ${BUILD_DIR}/openssl.tar.gz"
-rm -f "openssl.tar.gz"
-tar czf "openssl.tar.gz" iphoneos iphonesimulator macosx
+echo "Creating ${BUILD_DIR}/mitkerberos.tar.gz"
+rm -f "mitkerberos.tar.gz"
+tar czf "mitkerberos.tar.gz" macosx
 
-echo "Creating ${BUILD_DIR}/OpenSSL.xcframework.tar.gz"
-rm -f "OpenSSL.xcframework.tar.gz"
-tar czf "OpenSSL.xcframework.tar.gz" OpenSSL.xcframework
+echo "Creating ${BUILD_DIR}/MITKerberos.xcframework.tar.gz"
+rm -f "MITKerberos.xcframework.tar.gz"
+tar czf "MITKerberos.xcframework.tar.gz" MITKerberos.xcframework
 
 popd
